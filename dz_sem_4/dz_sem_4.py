1ab75d3e74a0d498ccd261f8509b6526c56a400c
KIRKA1356 <kgritsun@mail.ru>
2022-09-19 22:47:07 +0300

    4 задача

--- dz_sem_4/dz_sem_4.py
+++ dz_sem_4/dz_sem_4.py
@@ -54,11 +54,47 @@
 
 
 
-# 4- Шифр Цезаря - это способ шифрования, где каждая буква смещается на определенное количество символов влево или вправо. При расшифровке происходит обратная операция. К примеру, слово "абба" можно зашифровать "бввб" - сдвиг на 1 вправо. "вггв" - сдвиг на 2 вправо, "юяяю" - сдвиг на 2 влево.
-# Сдвиг часто называют ключом шифрования.
-# Ваша задача - написать функцию, которая записывает в файл шифрованный текст, а также функцию, которая спрашивает ключ, считывает текст и дешифровывает его.
-
 # Задача 4
+
+# def write_code():
+#     f = open('file2.txt', 'w')
+#     message = input("Сообщение для ДЕшифровки: ").upper()
+#     f.write(message)
+#     f.close
+
+# def deshifrovka_1():
+#     y = open('file2.txt', 'r')
+#     message_read = str(y.readline())
+#     y.close
+#     return message_read
+
+# def deshifrovka_2():
+#     itog = ''
+#     if lang == 'RU':
+#         for i in deshifrovka_1():
+#             mesto = alfavit_RU.find(i)
+#             new_mesto = mesto + smeshenie
+#             if i in alfavit_RU:
+#                 itog += alfavit_RU[new_mesto]
+#             else:
+#                 itog += i
+#     else:
+#         for i in deshifrovka_1():
+#             mesto = alfavit_EU.find(i)
+#             new_mesto = mesto + smeshenie
+#             if i in alfavit_EU:
+#                 itog += alfavit_EU[new_mesto]
+#             else:
+#                 itog += i
+#     return itog
+
+# alfavit_EU =  'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# alfavit_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+# lang = input('Выберите язык RU/EU: ')
+# write_code()
+# deshifrovka_1()
+# smeshenie = int(input('Шаг шифровки: '))
+# print (deshifrovka_2())
 
 
 
