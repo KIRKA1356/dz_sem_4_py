cb8350453e4f2a24efb8bcd5cb8955f39d7a789e
KIRKA1356 <kgritsun@mail.ru>
2022-09-19 14:34:48 +0300

    Сделал 1 и 2ую

---
+++ dz_sem_4/dz_sem_4.py
@@ -0,0 +1,29 @@
+
+# Задача 1
+
+# def simply_num():
+#     num = int(input("Введите число: "))
+#     list_num = []
+#     k = 0
+#     for i in range(2,num//2):
+#         if num % i == 0:
+#             if i % 2 !=0 and i % 5 !=0:
+#                 list_num.append(i)
+#             elif i==2:
+#                 list_num.append(2)
+#             elif i==5:
+#                 list_num.append(5)
+#     print(list_num)
+# simply_num()
+
+# Задача 2
+
+# list_num  = [1,1,1,1,2,2,2,3,6,7,8,7,3,3,4]
+# list_num_2 = []
+# for i in list_num:
+#     for j in list_num:
+#         if i not in list_num_2:
+#             list_num_2.append(i)
+# print(list_num_2)
+
+
